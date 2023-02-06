11d1c0437627b004db7ae0a521b3c72b12a66bfa
Muhammed Zafar <muhammedzafar.mm@gmail.com>
2023-02-06 19:11:13 +0530

    added gitignore

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
-from sys import argv
+from sys import argv, platform, stdout
+import os
 from rich import print
 from rich.align import Align
 
+
 def main():
+
+    print(
+        Align(
+            "This application uses emoji characters which may not be displayed correctly in terminals without Unicode support. For optimal experience, use a Unicode-supported terminal."
+        , align="center")
+    )
     # program checks tries to install all modules required to run the program
     try:
         # imports the program
@@ -10,6 +18,7 @@ def main():
 
         # starts the program
         initialise(argv)
+
         print(Align("Made with :heart:  by Zafar\n", align="center"))
 
     except ModuleNotFoundError:
