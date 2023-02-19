f15b2b6f01f0145bf2354e661ba60cea04ca76af
Muhammed Zafar <muhammedzafar.mm@gmail.com>
2023-02-19 21:15:42 +0530

    title card updates

--- sample/core.py
+++ sample/core.py
@@ -1,4 +1,6 @@
-import pyautogui, keyboard, threading
+import pyautogui
+import keyboard
+import threading
 
 from rich import print as rich_print
 from time import sleep
@@ -23,25 +25,29 @@ from .helpers import menu
 from rich.table import Table
 
 
-def l(text: str) -> None:
-    rich_print(Align(text, align="center"))
+title_card = [
+    ("[#F9ED69]               ,--,                                                               |          "),
+    ("[#F9ED69]            ,---.'|                                ,--.                          - -         "),
+    ("[#F9ED69]  ,----..   |   | :       ,---,   ,----..      ,--/  /|     ,---,. ,-.----.       |          "),
+    ("[#F9ED69] /   /   \  :   : |    ,`--.' |  /   /   \  ,---,': / '   ,'  .' | \    /  \           ,---, "),
+    ("[#F08A5D]|   :     : |   ' :    |   :  : |   :     : :   : '/ /  ,---.'   | ;   :    \         /_ ./| "),
+    ("[#F08A5D].   |  ;. / ;   ; '    :   |  ' .   |  ;. / |   '   ,   |   |   .' |   | .\ :   ,---, |  ' : "),
+    ("[#F08A5D].   ; /--`  '   | |__  |   :  | .   ; /--`  '   |  /    :   :  |-, .   : |: |  /___/ \.  : | "),
+    ("[#F08A5D];   | ;     |   | :.'| '   '  ; ;   | ;     |   ;  ;    :   |  ;/| |   |  \ :   .  \  \ ,' ' "),
+    ("[#B83B5E]|   : |     '   :    ; |   |  | |   : |     :   '   \   |   :   .' |   : .  /    \  ;  `  ,' "),
+    ("[#B83B5E].   | '___  |   |  ./  '   :  ; .   | '___  |   |    '  |   |  |-, ;   | |  \     \  \    '  "),
+    ("[#B83B5E]'   ; : .'| ;   : ;    |   |  ' '   ; : .'| '   : |.  \ '   :  ;/| |   | ;\  \     '  \   |  "),
+    ("[#B83B5E]'   | '/  : |   ,/     '   :  | '   | '/  : |   | '_\.' |   |    \ :   ' | \.'      \  ;  ;  "),
+    ("[#6A2C70]|   :    /  '---'      ;   |.'  |   :    /  '   : |     |   :   .' :   : :-'         :  \  \ "),
+    ("[#6A2C70] \   \ .'              '---'     \   \ .'   ;   |,'     |   | ,'   |   |.'            \  ' ; "),
+    ("[#6A2C70]  `---`                           `---`     '---'       `----'     `---'               `--`  ")
+]
 
-l("")
-l("[#F9ED69]               ,--,                                                               |          ")
-l("[#F9ED69]            ,---.'|                                ,--.                          - -         ")
-l("[#F9ED69]  ,----..   |   | :       ,---,   ,----..      ,--/  /|     ,---,. ,-.----.       |          ")
-l("[#F9ED69] /   /   \  :   : |    ,`--.' |  /   /   \  ,---,': / '   ,'  .' | \    /  \           ,---, ")
-l("[#F08A5D]|   :     : |   ' :    |   :  : |   :     : :   : '/ /  ,---.'   | ;   :    \         /_ ./| ")
-l("[#F08A5D].   |  ;. / ;   ; '    :   |  ' .   |  ;. / |   '   ,   |   |   .' |   | .\ :   ,---, |  ' : ")
-l("[#F08A5D].   ; /--`  '   | |__  |   :  | .   ; /--`  '   |  /    :   :  |-, .   : |: |  /___/ \.  : | ")
-l("[#F08A5D];   | ;     |   | :.'| '   '  ; ;   | ;     |   ;  ;    :   |  ;/| |   |  \ :   .  \  \ ,' ' ")
-l("[#B83B5E]|   : |     '   :    ; |   |  | |   : |     :   '   \   |   :   .' |   : .  /    \  ;  `  ,' ")
-l("[#B83B5E].   | '___  |   |  ./  '   :  ; .   | '___  |   |    '  |   |  |-, ;   | |  \     \  \    '  ")
-l("[#B83B5E]'   ; : .'| ;   : ;    |   |  ' '   ; : .'| '   : |.  \ '   :  ;/| |   | ;\  \     '  \   |  ")
-l("[#B83B5E]'   | '/  : |   ,/     '   :  | '   | '/  : |   | '_\.' |   |    \ :   ' | \.'      \  ;  ;  ")
-l("[#6A2C70]|   :    /  '---'      ;   |.'  |   :    /  '   : |     |   :   .' :   : :-'         :  \  \ ")
-l("[#6A2C70] \   \ .'              '---'     \   \ .'   ;   |,'     |   | ,'   |   |.'            \  ' ; ")
-l("[#6A2C70]  `---`                           `---`     '---'       `----'     `---'               `--`  ")
+
+def print_title():
+    for line in title_card:
+        rich_print(Align(line, align="center"))
+
 
 def initialise(argv):
     # Starts execution and handles keyboard interruption
@@ -139,11 +145,11 @@ def startup(argv):
     except pyautogui.FailSafeException:
         error("Execution Cancelled")
         return
-    
+
     except ExecutionStopped:
         error("Execution Stopped")
         return
-    
+
     except Exception as e:
         error(type(e).__name__)
         return
@@ -153,14 +159,16 @@ def startup(argv):
 
     # If all goes well...
     rich_print()
-    text = Align(":Party_Popper: [bold #8D72E1] SUCCESS :Party_Popper: ", align="center")
-    rich_print(Panel(text, subtitle="[#6D9886]The End", subtitle_align="right"))
+    text = Align(
+        ":Party_Popper: [bold #8D72E1] SUCCESS :Party_Popper: ", align="center")
+    rich_print(
+        Panel(text, subtitle="[#6D9886]The End", subtitle_align="right"))
 
 
 def post_record_menu():
     # The post record menu...
     print()
-    
+
     MENU = """\nPress [italic #332FD0]CTRL[/] to repeat a set of actions...
         Or Press [italic #332FD0]SHIFT[/] to replace any action...
                 Or Press [italic #332FD0]SPACE[/] to start execution...\n"""
@@ -172,35 +180,36 @@ def quit_operation():
     global status
     status = False
 
+
 def play_recorded():
-    
+
     global status
     status = True
-    
+
     keyboard.add_hotkey('ctrl + delete', quit_operation)
-    
+
     i = 0
 
     # Does what is recorded
     for action in config.record:
         for key, value in action.items():
-            
+
             if not status:
                 raise ExecutionStopped
-            
+
             recorded = Table(expand=True, box=None, highlight=True)
             recorded.add_column(justify="right")
             recorded.add_column(justify="center")
             recorded.add_column(justify="left")
-    
+
             position = value
             # For moving
             if key == "move":
-                
+
                 rich_print("[#7D9D9C italic]Moving to location...")
-                
+
                 i += 1
-                
+
                 pyautogui.moveTo(
                     position["x"],
                     position["y"],
@@ -209,16 +218,17 @@ def play_recorded():
                 )
 
                 print("\033[A                    \033[A")
-                
+
                 recorded.add_row(
                     ":palm_tree:",  f"[#829460 BOLD]MOVED TO[/#829460 BOLD] {position}", f"{i}"
                 )
-                
+
             # For Clicking
             elif key == "click":
-                
-                rich_print(f" [#7D9D9C italic]Inserting {action['click']} click...")
-                
+
+                rich_print(
+                    f" [#7D9D9C italic]Inserting {action['click']} click...")
+
                 i += 1
                 pyautogui.click(button=action['click'].lower())
                 current_position = {}
@@ -230,28 +240,30 @@ def play_recorded():
                 print("\033[A                        \033[A")
 
                 recorded.add_row(
-                    ":palm_tree:",  f"[#829460 BOLD]{action['click']} CLICKED AT [/#829460 BOLD]{current_position}".upper(), f"{i}"
+                    ":palm_tree:",  f"[#829460 BOLD]{action['click']} CLICKED AT [/#829460 BOLD]{current_position}".upper(
+                    ), f"{i}"
                 )
-                
+
             # For dragging with cursor
             elif key == "drag":
-                
+
                 rich_print(" [#7D9D9C italic]Dragging cursor to location...")
-                
+
                 i += 1
-                
-                pyautogui.dragTo(position["x"], position["y"], config.Drag_Speed)
+
+                pyautogui.dragTo(
+                    position["x"], position["y"], config.Drag_Speed)
                 print("\033[A                               \033[A")
                 recorded.add_row(
                     ":palm_tree:",  f"[#829460 BOLD]DRAGGED TO[/#829460 BOLD] {position}", f"{i}"
                 )
             # For text display
             elif key == "write":
-                
+
                 rich_print(" [#7D9D9C italic]Writing given text...")
-                
+
                 i += 1
-                
+
                 pyautogui.write(action["write"], interval=config.Type_Speed)
                 print("\033[A                     \033[A")
                 recorded.add_row(
@@ -259,11 +271,11 @@ def play_recorded():
                 )
             # For screen search
             elif key == "image":
-                
+
                 i += 1
-                
+
                 rich_print(" [#7D9D9C italic]Searching for given image...")
-                
+
                 try:
                     Image.open(action["image"]).convert("RGB").save(
                         r"assets\images\images.png"
@@ -271,20 +283,20 @@ def play_recorded():
                 except FileNotFoundError:
                     error("Image to wait for not found")
                     return
-                
+
                 detect_image(r"assets\images\images.png")
                 print("\033[A                          \033[A")
                 recorded.add_row(
                     ":palm_tree:",  f"[#829460 BOLD]FOUND[/#829460 BOLD] [italic #8D9EFF]{action['image']}", f"{i}"
                 )
-                
+
             # For wait
             elif key == "sleep":
-                
+
                 rich_print(" [#7D9D9C italic]Waiting for given time...")
-                
+
                 i += 1
-                
+
                 sleep(action["sleep"])
                 print("\033[A                          \033[A")
                 recorded.add_row(
@@ -292,11 +304,11 @@ def play_recorded():
                 )
             # For hotkey input
             elif key == "hotkey":
-                
+
                 rich_print(" [#7D9D9C italic]Inserting given hotkey...")
-                
+
                 i += 1
-                
+
                 for current_key in action["hotkey"]:
                     press(current_key)
                 for current_key in action["hotkey"]:
@@ -305,117 +317,116 @@ def play_recorded():
                 recorded.add_row(
                     ":palm_tree:",  f"[#829460 BOLD]INSERTED HOTKEYS[/#829460 BOLD] [italic #F0A500]{action['hotkey']}", f"{i}"
                 )
-                
+
             # For key input
             elif key == "key":
-                
+
                 i += 1
-                
+
                 rich_print(" [#7D9D9C italic]Inserting given key...")
-                
+
                 # Clicks key
                 time.sleep(0.3)
                 send(action["key"])
                 print("\033[A                       \033[A")
-                
+
                 key = action["key"]
-                
+
                 # For User
                 recorded.add_row(
                     ":palm_tree:",  f"[#829460 BOLD]INSERTED KEY[/#829460 BOLD][italic #F0A500] {key}", f"{i}"
                 )
-                
+
             # For key input
             elif key == "wait_key":
-                
+
                 rich_print(" [#7D9D9C italic]Waiting for given key input...")
-                
+
                 i += 1
-                
+
                 # waits for the given key
                 wait(action["wait_key"])
                 print("\033[A                              \033[A")
-                
+
                 key = action["wait_key"]
-                
+
                 # For User
                 recorded.add_row(
                     ":palm_tree:",  f"[#829460 BOLD]WAITED FOR[/#829460 BOLD][italic #F0A500] {key}", f"{i}"
                 )
-                    
+
             elif key == "screenshot":
-                
+
                 rich_print(" [#7D9D9C italic]Taking a screenshot...")
-                
+
                 i += 1
-                
+
                 pyautogui.screenshot(action["screenshot"])
                 print("\033[A                       \033[A")
                 recorded.add_row(
                     ":palm_tree:",  f"[#829460 BOLD]SCREENSHOT SAVED AT[/#829460 BOLD] [italic #8D9EFF]{action['screenshot']}", f"{i}"
                 )
-                
+
             else:
                 continue
-                
+
             rich_print(recorded)
             thread = Thread(target=playing_sound)
             thread.start()
-            
 
 
 def playing_sound():
-	playsound(r"assets\sounds\mixkit-arrow-whoosh-1491.wav")
-                
-                
+    playsound(r"assets\sounds\mixkit-arrow-whoosh-1491.wav")
+
+
 def detect_image(path):
     while True:
         image_location = pyautogui.locateCenterOnScreen(path, confidence=0.9)
         if image_location:
             pyautogui.moveTo(image_location)
             return
-      
-        
+
+
 def error(error=""):
     rich_print()
     text = Align(":cactus: [bold #8D72E1] FAILED [/] :cactus:", align="center")
     error = "[#6D9886]" + error
     rich_print(Panel(text, subtitle=error, subtitle_align="right"))
-    
+
 
 class ExecutionStopped(Exception):
     pass
 
-    
+
 def release_all():
     keys = ['\t', '\n', '\r', ' ', '!', '"', '#', '$', '%', '&', "'", '(',
-')', '*', '+', ',', '-', '.', '/', '0', '1', '2', '3', '4', '5', '6', '7',
-'8', '9', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`',
-'a', 'b', 'c', 'd', 'e','f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
-'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '{', '|', '}', '~',
-'accept', 'add', 'alt', 'altleft', 'altright', 'apps', 'backspace',
-'browserback', 'browserfavorites', 'browserforward', 'browserhome',
-'browserrefresh', 'browsersearch', 'browserstop', 'capslock', 'clear',
-'convert', 'ctrl', 'ctrlleft', 'ctrlright', 'decimal', 'del', 'delete',
-'divide', 'down', 'end', 'enter', 'esc', 'escape', 'execute', 'f1', 'f10',
-'f11', 'f12', 'f13', 'f14', 'f15', 'f16', 'f17', 'f18', 'f19', 'f2', 'f20',
-'f21', 'f22', 'f23', 'f24', 'f3', 'f4', 'f5', 'f6', 'f7', 'f8', 'f9',
-'final', 'fn', 'hanguel', 'hangul', 'hanja', 'help', 'home', 'insert', 'junja',
-'kana', 'kanji', 'launchapp1', 'launchapp2', 'launchmail',
-'launchmediaselect', 'left', 'modechange', 'multiply', 'nexttrack',
-'nonconvert', 'num0', 'num1', 'num2', 'num3', 'num4', 'num5', 'num6',
-'num7', 'num8', 'num9', 'numlock', 'pagedown', 'pageup', 'pause', 'pgdn',
-'pgup', 'playpause', 'prevtrack', 'print', 'printscreen', 'prntscrn',
-'prtsc', 'prtscr', 'return', 'right', 'scrolllock', 'select', 'separator',
-'shift', 'shiftleft', 'shiftright', 'sleep', 'space', 'stop', 'subtract', 'tab',
-'up', 'volumedown', 'volumemute', 'volumeup', 'win', 'winleft', 'winright', 'yen',
-'command', 'option', 'optionleft', 'optionright']
-    
+            ')', '*', '+', ',', '-', '.', '/', '0', '1', '2', '3', '4', '5', '6', '7',
+            '8', '9', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`',
+            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
+            'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '{', '|', '}', '~',
+            'accept', 'add', 'alt', 'altleft', 'altright', 'apps', 'backspace',
+            'browserback', 'browserfavorites', 'browserforward', 'browserhome',
+            'browserrefresh', 'browsersearch', 'browserstop', 'capslock', 'clear',
+            'convert', 'ctrl', 'ctrlleft', 'ctrlright', 'decimal', 'del', 'delete',
+            'divide', 'down', 'end', 'enter', 'esc', 'escape', 'execute', 'f1', 'f10',
+            'f11', 'f12', 'f13', 'f14', 'f15', 'f16', 'f17', 'f18', 'f19', 'f2', 'f20',
+            'f21', 'f22', 'f23', 'f24', 'f3', 'f4', 'f5', 'f6', 'f7', 'f8', 'f9',
+            'final', 'fn', 'hanguel', 'hangul', 'hanja', 'help', 'home', 'insert', 'junja',
+            'kana', 'kanji', 'launchapp1', 'launchapp2', 'launchmail',
+            'launchmediaselect', 'left', 'modechange', 'multiply', 'nexttrack',
+            'nonconvert', 'num0', 'num1', 'num2', 'num3', 'num4', 'num5', 'num6',
+            'num7', 'num8', 'num9', 'numlock', 'pagedown', 'pageup', 'pause', 'pgdn',
+            'pgup', 'playpause', 'prevtrack', 'print', 'printscreen', 'prntscrn',
+            'prtsc', 'prtscr', 'return', 'right', 'scrolllock', 'select', 'separator',
+            'shift', 'shiftleft', 'shiftright', 'sleep', 'space', 'stop', 'subtract', 'tab',
+            'up', 'volumedown', 'volumemute', 'volumeup', 'win', 'winleft', 'winright', 'yen',
+            'command', 'option', 'optionleft', 'optionright']
+
     for key in keys:
         pyautogui.keyUp(key)
-    
 
 
 if __name__ == "sample.core":
+    print_title()
     # Excecuted to increase the quality of ktinkter windows
     clarity()
