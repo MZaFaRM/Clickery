b96a7105b217c423de8a88f4fd05fe06f6ad305c
Muhammed Zafar <muhammedzafar.mm@gmail.com>
2023-02-19 21:30:08 +0530

    patch bugs

--- main.py
+++ main.py
@@ -1,27 +1,28 @@
 from sys import argv
-from rich import print
-from rich.align import Align
 
 
 def main():
 
-    print("This application uses emoji characters which may not be displayed " 
-        "correctly in terminals without Unicode support. For optimal " 
-        "experience, use a Unicode-supported terminal.")
 
     # program checks tries to install all modules required to run the program
     try:
         # imports the program
         from sample.core import initialise
+        from rich.align import Align
+        from rich import print as rich_print
 
+        print("This application uses emoji characters which may not be displayed " 
+            "correctly in terminals without Unicode support. For optimal " 
+            "experience, use a Unicode-supported terminal.")
+        
         # starts the program
         initialise(argv)
 
-        print(Align("Made with :heart:  by Zafar\n", align="center"))
+        rich_print(Align("Made with :heart:  by Zafar\n", align="center"))
 
     except ModuleNotFoundError:
 
-        print("\nPlease first run setup.py .")
+        print("\nPlease first run setup.py.")
 
 
 if __name__ == "__main__":
