d1a26fc7bee6f78c673d2d8b9322b8b1fba3c54e
Muhammed Zafar <muhammedzafar.mm@gmail.com>
2023-03-05 22:50:03 +0530

    Update menu.py

--- sample/helpers/menu.py
+++ sample/helpers/menu.py
@@ -33,7 +33,7 @@ def print_menu():
     panel_group = Group(
         Align("\nUsage:", align="center"),
         Align(
-            "[#ECDBBA]Hover over the screen and press the apropriate call",
+            "[#ECDBBA]Hover over the screen and press the appropriate call",
             align="center",
         ),
         Align("[#ECDBBA]to perform the related operation", align="center"),
